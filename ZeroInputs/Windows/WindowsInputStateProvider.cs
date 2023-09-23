2ebd431376a5bd055e1061bef3fbc8a27f3127b9
Dursun Zahid Korkmaz <dursunzahidkorkmaz@gmail.com>
2023-09-23 18:33:26 +0300

    Refactored WindowsInputStateProvider
    Corrected the namings and switched the properties with readonly fields

--- ZeroInputs/Windows/WindowsInputStateProvider.cs
+++ ZeroInputs/Windows/WindowsInputStateProvider.cs
@@ -7,8 +7,8 @@ internal sealed class WindowsInputStateProvider : IInputStateProvider
     internal const int VirtualKeyCount = 256;
     private const string User32 = "user32.dll";
 
-    private byte[] PreviousStates { get; } = new byte[VirtualKeyCount];
-    private byte[] CurrentStates { get; } = new byte[VirtualKeyCount];
+    private readonly byte[] _previousStates = new byte[VirtualKeyCount];
+    private readonly byte[] _currentStates = new byte[VirtualKeyCount];
 
     // @alihakankurt TODO: Extract library imports to a seperate class and make it static to avoid multiple imports.
     #region LibraryImports
@@ -24,22 +24,22 @@ internal sealed class WindowsInputStateProvider : IInputStateProvider
     {
         // This is somehow required to get the correct state of the keys.
         GetKeyState(0); 
-        CurrentStates.CopyTo(PreviousStates, 0);
-        GetKeyboardState(CurrentStates);
+        _currentStates.CopyTo(_previousStates, 0);
+        GetKeyboardState(_currentStates);
     }
     #endregion
 
     #region KeyInformation
     public bool IsKeyDown(ushort keyCode)
-       => (CurrentStates[keyCode] & 0x80) != 0;
+       => (_currentStates[keyCode] & 0x80) != 0;
 
     public bool IsKeyPressed(ushort keyCode)
-        => (CurrentStates[keyCode] & 0x80) != 0 && (PreviousStates[keyCode] & 0x80) == 0;
+        => (_currentStates[keyCode] & 0x80) != 0 && (_previousStates[keyCode] & 0x80) == 0;
 
     public bool IsKeyReleased(ushort keyCode)
-        => (CurrentStates[keyCode] & 0x80) == 0 && (PreviousStates[keyCode] & 0x80) != 0;
+        => (_currentStates[keyCode] & 0x80) == 0 && (_previousStates[keyCode] & 0x80) != 0;
 
     public bool IsTogglableKeyOn(ushort keyCode)
-        => (CurrentStates[keyCode] & 0x0001) != 0;
+        => (_currentStates[keyCode] & 0x0001) != 0;
     #endregion
 }
